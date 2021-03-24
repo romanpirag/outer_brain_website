e3f7756f2a76d460290d04aa92266a77774ce7af
Kory Powell <actual@korypowell.dev>
2021-03-24 05:42:11 -0700

    First live site commit with build + docs symlink

---
+++ build/precache-manifest.258dc7c2d8e7503c104d9a653c5bde8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495db7ba3507a062f27749cd0acea637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188fb068d38d8bbe176c",
+    "url": "/static/css/main.ad40a687.chunk.css"
+  },
+  {
+    "revision": "990d05e0b097cbc57982",
+    "url": "/static/js/2.55e810db.chunk.js"
+  },
+  {
+    "revision": "188fb068d38d8bbe176c",
+    "url": "/static/js/main.5147fb31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
