e5d22b7ea293d867a2036e2de5e3324c440eadae
Mark Rothberg <55262979+MarkR37@users.noreply.github.com>
2024-05-28 18:20:28 -0400

    Added Twitch Authenticator

---
+++ src/MainPage.js
@@ -0,0 +1,28 @@
+import React from "react"
+import "./App.css"
+import HomePage from "./Components/HomePage.js"
+import { TopLinks } from "./TopLinks"
+
+function MainPage() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <TopLinks />
+      </header>
+
+      <main className="main">
+        <HomePage />
+      </main>
+
+      {/* <footer>
+        <h1 className="copyright">
+          {" "}
+          &copy; Outer Brain all rights reserved 2019
+        </h1>
+      </footer> */}
+    </div>
+  )
+}
+
+export default MainPage
